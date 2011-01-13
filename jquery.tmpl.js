0daf056bb51927b0298e6502bc2c158972fda68c
Konstantin Pozin <kpozin@gmail.com>
2011-01-13 10:38:42 -0500

    Added special handling for data fields that are jQuery objects; these are converted to HTML (using an outerHTML hack) instead of just passing them in as [Object object].
    Depends on this .outerHTML extension adapted from http://yelotofu.com/2008/08/jquery-outerhtml/
    
        jQuery.fn.outerHTML = function(s) {
            return (s) ? this.before(s).remove() : jQuery("<p>").append(this.clone()).html();
        };

--- jquery.tmpl.js
+++ jquery.tmpl.js
@@ -27,6 +27,17 @@
 			html: tiHtml,
 			update: tiUpdate
 		};
+        
+        var dataProps = Object.getOwnPropertyNames(newItem.data);
+        for (var i = 0; i < dataProps.length; ++i) {
+            var propName = dataProps[i];
+            var datum = newItem.data[propName];
+            if (datum && datum instanceof jQuery) {
+                datum = datum.outerHTML();
+                newItem.data[propName] = datum;
+            }
+        }
+        
 		if ( options ) {
 			jQuery.extend( newItem, options, { nodes: [], parent: parentItem });
 		}
